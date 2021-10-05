e78c29f3f5fef6a501ce9efa5b7b7ff6b7f75887
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-10-05 19:35:47 +0300

    feat: models + routes + connect mongo + i body-parser, mongoose

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,18 @@
-const express = require("express");
-const { PORT = 3000 } = process.env;
-const mongoose = require("mongoose"); // для подключения к БД
+const User = require("../models/user");
 
-const app = express();
-mongoose.connect("mongodb://localhost:27017/mestodb");
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
-
-const User = require("./models/user");
-
-app.get("/users/", (req, res) => {
+router.get("/users/", (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
     .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
 });
 
-app.get("/users/:userId", (req, res) => {
+router.get("/users/:userId", (req, res) => {
   User.findById(req.params.id)
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
 });
 
-app.post("/users", (req, res) => {
+router.post("/users", (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })

0dd9a57e6b0fb48528e42e4a0ec532c74fa324dc
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-10-06 15:54:57 +0300

    refactor: app.js

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
 const express = require("express");
 const { PORT = 3000 } = process.env;
-const mongoose = require("mongoose"); // для подключения к БД
-
 const app = express();
+const router = require("./routes"); // импортируем роутер
+
+// для подключения к БД
+const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/mestodb");
+
+// временный костыль по ТЗ
+app.use((req, res, next) => {
+  req.user = {
+    _id: "615c7d2fd1e07a1d59a48cb9",
+  };
+  next();
+});
+
+app.use(express.json());
+
+app.use(router); // запускаем роутер
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
-});
-
-const User = require("./models/user");
-
-app.get("/users/", (req, res) => {
-  User.find({})
-    .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
-});
-
-app.get("/users/:userId", (req, res) => {
-  User.findById(req.params.id)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
-});
-
-app.post("/users", (req, res) => {
-  const { name, about, avatar } = req.body;
-
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
 });

8248e826e405fa5d24e7ee9346e5251b46421e5c
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-10-06 15:54:40 +0300

    refactor: routes users

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,32 @@
-const User = require("../models/user");
+const router = require("express").Router(); // создали роутер
+const User = require("../models/user"); // импортируем модель
 
-router.get("/users/", (req, res) => {
+router.get("/", (req, res) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
+    .then((users) => {
+      res.send({ data: users });
+      // console.log("users");
+    })
     .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
 });
 
-router.get("/users/:userId", (req, res) => {
-  User.findById(req.params.id)
-    .then((user) => res.send({ data: user }))
+router.get("/:userId", (req, res) => {
+  const { userId } = req.params;
+
+  User.findById(userId)
+    .then((user) => {
+      res.send({ data: user });
+      // console.log("userid");
+    })
     .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
 });
 
-router.post("/users", (req, res) => {
+router.post("/", (req, res) => {
   const { name, about, avatar } = req.body;
-
+  // console.log(req.body);
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
 });
+
+module.exports = router; // экспортировали роутер
